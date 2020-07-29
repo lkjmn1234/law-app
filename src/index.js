294ef382d5f046bfa672d05a7a88b2f34f545b71
CLL319 <cll319@ha.org.hk>
2020-07-29 14:38:28 +0800

    heroku

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ import './assets/scss/style.scss';
 
 const history = createBrowserHistory();
 //<!-- Hotjar Tracking Code for https://lawinfo-app.herokuapp.com/ -->
-(function(h,o,t,j,a,r){
-	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	h._hjSettings={hjid:1922595,hjsv:6};
-	a=o.getElementsByTagName('head')[0];
-	r=o.createElement('script');r.async=1;
-	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 ReactDOM.render(
   <Router history={history}>
     <App />
