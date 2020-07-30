cded89f0c97e1c6a676c21685973ebab92c9384d
CLL319 <cll319@ha.org.hk>
2020-07-30 11:46:24 +0800

    heroku

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch } from 'react-router-dom';
-import AppRoute from './utils/AppRoute';
+import { useLocation, Switch ,Route} from 'react-router-dom';
 import ScrollReveal from './utils/ScrollReveal';
 
-// Layouts
-import LayoutDefault from './layouts/LayoutDefault';
 
-// Views 
+// Views
 import Home from './views/Home';
 
 const App = () => {
@@ -15,7 +12,6 @@ const App = () => {
   let location = useLocation();
 
   useEffect(() => {
-    const page = location.pathname;
     document.body.classList.add('is-loaded')
     childRef.current.init();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -26,7 +22,7 @@ const App = () => {
       ref={childRef}
       children={() => (
         <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <Route exact path="/" component={Home} />
         </Switch>
       )} />
   );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 const Home = () => {
 
   return (
     <>
+      <Header navPosition="right" className="reveal-from-bottom" />
+      <main className="site-content">
+      </main>
+      <Footer />
     </>
   );
 }
