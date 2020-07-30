c4f7db7e94d09d8134f1d7f345ef4ce58a334541
CLL319 <cll319@ha.org.hk>
2020-07-30 19:05:37 +0800

    heroku

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,18 +1,14 @@
 import React from 'react';
 // import sections
-import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
 
-const Home = () => {
+const Login = () => {
 
     return (
         <>
-            <FeaturesTiles/>
             <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02"/>
-            <Testimonial topDivider/>
         </>
     );
 }
 
-export default Home;
+export default Login;
