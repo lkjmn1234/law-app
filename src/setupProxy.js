feb0e05f8771cd691b4295ee548d6e4cb18b6d59
Louis.Chan <louishahahaha@gmail.com>
2020-08-02 00:17:46 +0800

    first commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,21 +3,21 @@ module.exports = function(app) {
   app.use(
     '/interactive.jpg',
     createProxyMiddleware({
-      target: 'http://localhost:3000/interactive.jpg',
+      target: 'http://localhost:3000',
       changeOrigin: true,
     })
   );
   app.use(
     '/informative.jpg',
     createProxyMiddleware({
-      target: 'http://localhost:3000/informative.jpg',
+      target: 'http://localhost:3000',
       changeOrigin: true,
     })
   );
   app.use(
     '/intelligent.jpg',
     createProxyMiddleware({
-      target: 'http://localhost:3000/intelligent.jpg',
+      target: 'http://localhost:3000',
       changeOrigin: true,
     })
   );
