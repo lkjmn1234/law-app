795852327683da354d825b00906cfc60e650a1e2
Louis.Chan <louishahahaha@gmail.com>
2020-08-01 23:58:11 +0800

    first commit

---
+++ public/precache-manifest.2ef54ee8c6b8a72413e0689fc8ae78dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1ab4585604deaeffcc17806c7d47ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd07f8bd03a916883400",
+    "url": "/static/css/main.73a2caa2.chunk.css"
+  },
+  {
+    "revision": "9a4244e6133e49dff6e9",
+    "url": "/static/js/2.8d9ab06a.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.8d9ab06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd07f8bd03a916883400",
+    "url": "/static/js/main.6ba6abb4.chunk.js"
+  },
+  {
+    "revision": "8dff504f4d70eeea4723",
+    "url": "/static/js/runtime-main.61756e84.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
