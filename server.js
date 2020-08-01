d19cde8c4f286fa222517c01e81696d78859e456
Louis.Chan <louishahahaha@gmail.com>
2020-08-02 00:36:46 +0800

    first commit

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/interactive.JPG', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'interactive.JPG'));
+});
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
