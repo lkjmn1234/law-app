381b329aaf99db7208a9c9cf699c683ef1a718fe
Louis.Chan <louishahahaha@gmail.com>
2020-08-01 23:29:19 +0800

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
+
+
+app.get('/interactive.jpg', function(req, res) {
+  res.sendFile(path.join(__dirname,'build',  'interactive.jpg'));
+});
+app.get('/informative.jpg', function(req, res) {
+  res.sendFile(path.join(__dirname,'build', 'informative.jpg'));
+});
+app.get('/intelligent.jpg', function(req, res) {
+  res.sendFile(path.join(__dirname,'build', 'intelligent.jpg'));
+});
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
